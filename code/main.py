59340dab4d51a39e1fedce2ff62fb74438773f7a
rishicds <124495375+rishicds@users.noreply.github.com>
2024-03-13 23:39:14 +0530

    sa

--- code/main.py
+++ code/main.py
@@ -3,36 +3,52 @@ from settings import *
 from level import Level
 
 class Game:
-	def __init__(self):
+    def __init__(self):
 
-		# general setup
-		pygame.init()
-		self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
-		pygame.display.set_caption('Zelda')
-		self.clock = pygame.time.Clock()
+        # general setup
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGTH))
+        pygame.display.set_caption('Zelda DarkSouls')
+        self.clock = pygame.time.Clock()
 
-		self.level = Level()
+        self.level = Level()
 
-		# sound 
-		main_sound = pygame.mixer.Sound('MLHGW_PyGame_Zelda/audio/main.ogg')
-		main_sound.set_volume(0.5)
-		main_sound.play(loops = -1)
-	
-	def run(self):
-		while True:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					sys.exit()
-				if event.type == pygame.KEYDOWN:
-					if event.key == pygame.K_m:
-						self.level.toggle_menu()
+        # sound 
+        main_sound = pygame.mixer.Sound('MLHGW_PyGame_Zelda/audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops = -1)
 
-			self.screen.fill(WATER_COLOR)
-			self.level.run()
-			pygame.display.update()
-			self.clock.tick(FPS)
+        # splash screen
+        self.show_splash_screen()
+
+    def show_splash_screen(self):
+        # Load splash screen image
+        splash_image = pygame.image.load('MLHGW_PyGame_Zelda/images/splash2.png')
+
+        # Draw splash screen
+        self.screen.blit(splash_image, (0, 0))
+
+        # Update display
+        pygame.display.flip()
+
+        # Wait for a certain amount of time
+        pygame.time.wait(3000)  # 3000 milliseconds = 3 seconds
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_m:
+                        self.level.toggle_menu()
+
+            self.screen.fill(WATER_COLOR)
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
 
 if __name__ == '__main__':
-	game = Game()
-	game.run()
+    game = Game()
+    game.run()
